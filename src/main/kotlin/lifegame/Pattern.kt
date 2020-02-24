729312e5d6427c718491f0ae155837788fa52ce5
Matias Lev <matias.lev@globant.com>
2020-02-24 14:23:03 -0300

    patterns added

--- src/main/kotlin/lifegame/Pattern.kt
+++ src/main/kotlin/lifegame/Pattern.kt
@@ -6,6 +6,10 @@ sealed class Pattern(val positions: List<Tuple2<Int, Int>>) {
     object blinker: Pattern(listOf(Tuple2(5, 4), Tuple2(5, 5), Tuple2(5, 6)))
     object beacon: Pattern(listOf(Tuple2(4, 4), Tuple2(4, 5), Tuple2(5, 4), Tuple2(6, 7), Tuple2(7, 6), Tuple2(7, 7)))
     object glider: Pattern(listOf(Tuple2(4, 4), Tuple2(4, 5), Tuple2(4, 6), Tuple2(5, 4), Tuple2(6, 5)))
+    object toad: Pattern(listOf(Tuple2(4,5), Tuple2(4,6), Tuple2(4, 7), Tuple2(5,4), Tuple2(5,5), Tuple2(5, 6)))
+    object diehard: Pattern(listOf(Tuple2(4, 7), Tuple2(5, 1), Tuple2(5, 2), Tuple2(6, 2), Tuple2(6 ,6), Tuple2(6,7), Tuple2(6, 8)))
+    object acorn: Pattern(listOf(Tuple2(4,2), Tuple2(5,4), Tuple2(6,1), Tuple2(6,2), Tuple2(6, 5), Tuple2(6,6), Tuple2(6, 7)))
+    object pentomino: Pattern(listOf(Tuple2(4,5), Tuple2(4,6), Tuple2(5,4), Tuple2(5, 5), Tuple2(6,5)))
 
     // [(4, 4), (4, 5), (4, 6), (5, 4), (6, 5)]
 
